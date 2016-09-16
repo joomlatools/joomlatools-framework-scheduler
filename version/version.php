d9c4de6909e15f36183f9707070fc2512f3c5916
Ercan Ozkaya <ercan@timble.net>
2016-09-16 15:59:58 +0300

    Add version object

---
+++ version/version.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomlatools Scheduler
+ * @copyright   Copyright (C) 2016 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.joomlatools.com
+ */
+
+class ComSchedulerVersion extends KObject
+{
+    const VERSION = '1.0.0';
+
+    /**
+     * Get the version
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return self::VERSION;
+    }
+}
