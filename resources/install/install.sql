a35c71d352cb1ae255359093f266183806b72e7a
Ercan Ozkaya <ercan@timble.net>
2022-01-03 16:07:07 +0300

    #9: Do not use zero dates

--- resources/install/install.sql
+++ resources/install/install.sql
@@ -7,15 +7,27 @@ CREATE TABLE IF NOT EXISTS `#__scheduler_jobs` (
   `queue` tinyint(4) NOT NULL DEFAULT '0',
   `ordering` int(11) NOT NULL DEFAULT '0',
   `state` text,
-  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `completed_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_on` datetime DEFAULT NULL,
+  `completed_on` datetime DEFAULT NULL,
   UNIQUE KEY `name` (`identifier`),
   UNIQUE KEY `uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `#__scheduler_metadata` (
   `type` varchar(32) NOT NULL DEFAULT '',
-  `sleep_until` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `last_run` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `sleep_until` datetime DEFAULT NULL,
+  `last_run` datetime DEFAULT NULL,
   UNIQUE KEY `unique_type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `#__scheduler_jobs` CHANGE `created_on` `created_on` datetime DEFAULT NULL;
+ALTER TABLE `#__scheduler_jobs` CHANGE `modified_on` `modified_on` datetime DEFAULT NULL;
+
+UPDATE `#__scheduler_jobs` SET `created_on` = NULL WHERE `created_on` = 0000-00-00;
+UPDATE `#__scheduler_jobs` SET `modified_on` = NULL WHERE `modified_on` = 0000-00-00;
+
+ALTER TABLE `#__scheduler_metadata` CHANGE `sleep_until` `sleep_until` datetime DEFAULT NULL;
+ALTER TABLE `#__scheduler_metadata` CHANGE `last_run` `last_run` datetime DEFAULT NULL;
+
+UPDATE `#__scheduler_metadata` SET `sleep_until` = NULL WHERE `sleep_until` = 0000-00-00;
+UPDATE `#__scheduler_metadata` SET `last_run` = NULL WHERE `last_run` = 0000-00-00;
